fd865049f9ede33ade885ffefad6d488780b4549
Kauê Nascimento <iksn7@icloud.com>
2020-06-06 17:06:26 -0300

    Created whole backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,9 +7,10 @@ const routes = express.Router();
 const pointsController = new PointsController;
 const itemsController = new ItemsController;
 
-routes.post('/points', pointsController.create)
 routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create)
 routes.get('/points/:id',pointsController.show)
-routes.get('/points',pointsController.show)
+routes.get('/points',pointsController.index)
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes';
 import path from 'path';
+import cors from 'cors';
 //Rota: Endereço completo da requisição
 //Recurso: Qual entidade estamos acessando no sistema
 
@@ -12,8 +13,8 @@ import path from 'path';
 //Request Param: Parâmetros que vem  na própria rota que identificam um recurso.
 //Query Param: Parâmetros que vem na própria rota, geralmente são opcionais, para filtros e paginações.
 //Request Body: Parâmetros para  criação/atualização de informações 
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes); 
 app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')))
